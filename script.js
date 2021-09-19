1f8842a4fd9fc23284206c07d47758bc98e041d2
awadhy <awadhym@outlook.com>
2021-09-19 17:50:48 +0100

    Tidied up code, and added return in case of tie to if conditionals. Also added in 2 new global variables which will be useful for determining winner

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+ // Global variables in order to pass parameters to the functions.
+
+ let playerInput = prompt("Enter your move: Rock, Paper or Scissors", "");
+ let playerSelection = playerInput.toLowerCase();
+ const computerSelection = computerPlay();   
+ let playerScore;
+ let computerScore; 
+
+// Function for the computer to randomly generate a move.
+
 function computerPlay() {
     let move = ["Rock", "Paper", "Scissors"];
     let randomMove = [Math.floor(Math.random() * move.length)];
@@ -6,7 +16,7 @@ function computerPlay() {
     }
 }
 
-// computerSelection = computerPlay();
+// Function that plays a single round and returns a win/loss message 
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "Scissors") {
@@ -21,12 +31,15 @@ function playRound (playerSelection, computerSelection) {
         return "You lose! Scissors beats Paper";
     } else if (playerSelection == "rock" && computerSelection == "Paper") {
         return "You lose! Paper beats Rock";
-    }
+   } else if (playerSelection == "rock" && computerSelection == "Rock") {
+       return "It's a tie. You both drew Rock";
+   } else if (playerSelection == "paper" && computerSelection == "Paper") {
+       return "It's a tie. You both drew Paper";
+   } else if (playerSelection == "scissors" && computerSelection == "Scissors") {
+       return "It's a tie. You both drew Scissors";
    }
+}
 
    
-   let playerInput = prompt("Enter your move: Rock, Paper or Scissors", "");
-   const playerSelection = playerInput.toLowerCase();
-   const computerSelection = computerPlay();
-   console.log(playRound(playerSelection, computerSelection));
+// testing the round: console.log(playRound(playerSelection, computerSelection));
 
