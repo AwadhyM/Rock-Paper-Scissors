48d451b2d77931699133761bc396852f961b129e
awadhy <awadhym@outlook.com>
2021-09-19 19:22:07 +0100

    Made changes so my conditial statement in playRound function was a lot shorter and could return a tie message

--- script.js
+++ script.js
@@ -3,43 +3,48 @@
  let playerInput = prompt("Enter your move: Rock, Paper or Scissors", "");
  let playerSelection = playerInput.toLowerCase();
  const computerSelection = computerPlay();   
- let playerScore;
- let computerScore; 
+ let playerScore = 0;
+ let computerScore = 0;
 
 // Function for the computer to randomly generate a move.
 
 function computerPlay() {
-    let move = ["Rock", "Paper", "Scissors"];
+    let move = ["rock", "paper", "scissors"];
     let randomMove = [Math.floor(Math.random() * move.length)];
-    if (randomMove == 0 || randomMove == 1 || randomMove == 2) {
         return move[randomMove];
     }
-}
+
+
+
 
 // Function that plays a single round and returns a win/loss message 
 
 function playRound (playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "Scissors") {
+    if (playerSelection == "rock" && computerSelection == "scissors") {
         return "You win! Rock beats Scissors!";
-    } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
         return "You win! Scissors beats Paper!";
-    } else if (playerSelection == "paper" && computerSelection == "Rock") {
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
         return "You win! Paper beats Rock!";
-    } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
         return "You lose! Rock beats Scissors!";
-    } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
         return "You lose! Scissors beats Paper";
-    } else if (playerSelection == "rock" && computerSelection == "Paper") {
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
         return "You lose! Paper beats Rock";
-   } else if (playerSelection == "rock" && computerSelection == "Rock") {
-       return "It's a tie. You both drew Rock";
-   } else if (playerSelection == "paper" && computerSelection == "Paper") {
-       return "It's a tie. You both drew Paper";
-   } else if (playerSelection == "scissors" && computerSelection == "Scissors") {
-       return "It's a tie. You both drew Scissors";
+   } else if (playerSelection == computerSelection) {
+       return  "It's a tie. You both drew the same move";
    }
 }
 
-   
-// testing the round: console.log(playRound(playerSelection, computerSelection));
+ console.log(playerSelection);
+ console.log(computerSelection);
+ console.log(playRound(playerSelection, computerSelection));
 
+// game function which will within it invoke playRound
+
+function game() {
+    let playerInput1 = prompt("Enter your move for Round 1: Rock, Paper or Scissors", "");
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection,computerSelection));
+}
