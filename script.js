e8e24a203f31756bf92a82661a504879052bb08d
awadhy <awadhym@outlook.com>
2021-09-19 12:04:11 +0100

    Created function for the computer to randomly generate a move. Tested with console.log() and got the expected output.

---
+++ script.js
@@ -0,0 +1,9 @@
+function computerPlay() {
+    let move = ["Rock", "Paper", "Scissors"];
+    let randomMove = [Math.floor(Math.random() * move.length)];
+    if (randomMove == 0 || randomMove == 1 || randomMove == 2) {
+        return "Computer played " + move[randomMove];
+    }
+}
+
+console.log(computerPlay());
